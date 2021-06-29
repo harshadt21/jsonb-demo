9f0e5afd2e4ae8245fce53e854e9072c0c60174b
Harshad Vijay Thombare <harshadthombare21@gmail.com>
2021-06-30 00:54:06 +0530

    Update ddl for assets

--- src/assets.sql
+++ src/assets.sql
@@ -2,6 +2,7 @@ create table assets
 (
     id            serial,
     name          varchar,
+    brand          varchar,
     serial_number varchar,
     specifications    jsonb,
     PRIMARY KEY (id)
