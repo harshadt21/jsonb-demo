84dfa92351544d4ed49d53d2cf03374addb5eb37
Harshad Vijay Thombare <harshadthombare21@gmail.com>
2021-06-30 00:54:54 +0530

    Add sql queries

--- src/queries.sql
+++ src/queries.sql
@@ -2,7 +2,7 @@
 select * from assets;
 
 -- View specific column of all records
-select name, serial_number, specifications -> 'brand' as brand, specifications -> 'countryOfOrigin' as origin from assets;
+select name, serial_number, brand, specifications -> 'countryOfOrigin' as origin from assets;
 
 -- View specific record using where clause
 select * from assets where specifications @> '{"countryOfOrigin":"China"}';
@@ -12,3 +12,12 @@ select * from assets where specifications ->> 'modelYear' <= '2018';
 -- view record if given column is present
 -- select * from assets where specifications ? 'processorType';
 -- select * from assets where specifications ?| array['processorType', 'operatingSystem'];
+
+
+-- List all keyboards which has type @>
+select * from assets where name='keyboard' and specifications @> '{"connectorType": "Wired"}';
+
+-- List all laptops which has ram greater than 8gb ->
+select * from assets where name='laptop' and (specifications ->> 'ramSizeInGB')::INTEGER > 8;
+
+
