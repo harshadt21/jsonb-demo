f79d2ccafff41509d346a1b55f4c461f96faf6c6
Harshad Vijay Thombare <harshadthombare21@gmail.com>
2021-08-02 19:16:26 +0530

    Update sql queries for demo

--- src/demo.sql
+++ src/demo.sql
@@ -1,26 +1,65 @@
 -- Definition of table "assets"
-select column_name, data_type, character_maximum_length, column_default, is_nullable
-from INFORMATION_SCHEMA.COLUMNS
-where table_name = 'assets';
+select column_name, data_type, character_maximum_length, column_default, is_nullable from INFORMATION_SCHEMA.COLUMNS where table_name = 'assets';
 
 
--- Sample data is "assets" table
-select *
-from assets;
-select count(*)
-from assets;
 
--- fetch laptops with 16 gb ram
--- Fetch assets with color navy blue using extraction operator
-select *
-from assets
-where specifications ->> 'color' = 'Navy Blue';
 
--- fetch assets with color white using full text search
-select *
-from assets
-where specifications @> '{"colour":"White"}';
 
+
+
+-- Sample data is "assets" table, total records are 1 million
+select * from assets limit 5;
+
+
+
+
+
+
+
+-- Fetch laptops with 16 gb ram using extraction operator
+select * from assets where (specifications ->> 'ramSizeInGB')::Integer = 16;
+
+
+
+
+
+
+
+-- Fetch assets with colour navy blue using extraction operator // dont run the query
+select * from assets where specifications ->> 'colour' = 'Navy Blue';
+
+
+
+
+
+
+
+-- fetch assets with colour white using full text search (contains)
+select * from assets where specifications @> '{"colour":"White"}';
+
+
+
+
+
+
+
+-- Formatting jsonb fields as columns (talk about group by as well)
+select name,
+       brand,
+       specifications -> 'colour'        as color,
+       specifications -> 'ramSizeInGB'   as "ram in GB",
+       specifications -> 'hardDriveSize' as storage,
+       count(*)
+from assets 
+group by name, brand, specifications limit 5;
+
+
+
+
+
+
+
+-- Filtering out records which does not have ramSizeInGB field
 select name,
        brand,
        specifications -> 'colour'        as color,
@@ -28,62 +67,71 @@ select name,
        specifications -> 'hardDriveSize' as storage,
        count(*)
 from assets
-where name != 'Software'
-group by name, brand, specifications;
+where specifications ? 'ramSizeInGB'
+group by name, brand, specifications limit 5;
 
-select name,
-       brand,
-       specifications -> 'colour'        as color,
-       specifications -> 'ramSizeInGB'   as "ram in GB",
-       specifications -> 'hardDriveSize' as storage,
-       count(*)
-from assets
-where name != 'Software'
-  and specifications ? 'ramSizeInGB'
-group by name, brand, specifications;
+
+
+
+
+
 
 -- Total amount invested in assets
 select sum(cast(specifications ->> 'price' as decimal)) from assets;
 
+
+
+
+
+
+
 -- Average price of a laptop in inventory
 select avg(cast(specifications ->> 'price' as decimal)) from assets where name='laptop';
 
+
+
+
+
+
+
+-- Gin index using jsonb_ops
+explain analyze select * from assets where specifications @> '{"colour":"White"}'::jsonb;
+explain analyse select * from assets where specifications @> '{"ramSizeInGB": "4" }'::jsonb;
+
+create index idx_specifications_jsonops on assets using gin(specifications JSONB_OPS);
+
+explain analyze select * from assets where specifications @> '{"colour":"White"}'::jsonb;
 explain  analyse select * from assets where specifications @> '{"ramSizeInGB": "4" }'::jsonb;
--- select * from assets where (specifications ->> 'ramSizeInGB') =  '4';
 
-explain analyze
-select *
-from assets
-where specifications @> '{"colour":"White"}'::jsonb;
+drop index idx_specifications_jsonops;
 
-create index idx_specifications on assets using gin(specifications JSONB_OPS);
 
-explain analyze
-select *
-from assets
-where specifications @> '{"colour":"White"}'::jsonb;
 
-drop index idx_specifications;
 
+
+
+
+-- GIN expression indexing
 explain analyse select * from assets where specifications @> '{"colour":"White"}'::jsonb;
 explain analyse select * from assets where specifications -> 'colour' ? 'White';
 explain analyse select * from assets where specifications ? 'colour';
 
-create index idx_ram on assets using gin((specifications -> 'colour'));
+create index idx_expression on assets using gin((specifications -> 'colour'));
 drop index idx_ram;
 
 
 
-explain analyze
-select *
-from assets
-where specifications @> '{"colour":"White"}'::jsonb;
+
+
+
+
+-- GIN indexing using jsonb_path_ops
+explain analyze select * from assets where specifications @> '{"colour":"White"}'::jsonb;
+select * from assets where specifications @> '{"colour":"White"}'::jsonb;
 
 create index idx_specifications_1 on assets using gin(specifications JSONB_PATH_OPS);
 
-explain analyze
-select *
-from assets
-where specifications @> '{"colour":"White"}'::jsonb;
+explain analyze select * from assets where specifications @> '{"colour":"White"}'::jsonb;
+select * from assets where specifications @> '{"colour":"White"}'::jsonb;
 
 drop index idx_specifications_1;
